91c5efcbe00e88dce721008ec6487b6eef61f4fc
suho0515 <suho0515@gmail.com>
2021-10-19 21:04:25 +0900

    Update ur_point_node.py

--- src/ur_point_node.py
+++ src/ur_point_node.py
@@ -29,7 +29,9 @@ from ur_dashboard_msgs.srv import GetRobotMode, \
 
 # controller manager service messages to choose controller for ur robot
 from controller_manager_msgs.srv import SwitchControllerRequest, \
-                                        SwitchController
+                                        SwitchController, \
+                                        LoadControllerRequest, \
+                                        LoadController
 
 # Trigger Module for standard service
 from std_srvs.srv import Trigger
@@ -121,6 +123,18 @@ class UR():
             print(
                 "Could not reach set_mode action. Make sure that the driver is actually running."
                 " Msg: {}".format(err))
+            
+        # Connect to Load Controller Action Server from Client
+        self.load_controllers_client = rospy.ServiceProxy('/controller_manager/load_controller',
+                LoadController)
+        try:
+            self.Load_controllers_client.wait_for_service(timeout)
+            print("controller load service is on")
+        except rospy.exceptions.ROSException as err:
+            print(
+                "Could not reach controller load service. Make sure that the driver is actually running."
+                " Msg: {}".format(err))
+            
         # Connect to Switch Controller Action Server from Client
         self.switch_controllers_client = rospy.ServiceProxy('/controller_manager/switch_controller',
                 SwitchController)
@@ -131,6 +145,7 @@ class UR():
             print(
                 "Could not reach controller switch service. Make sure that the driver is actually running."
                 " Msg: {}".format(err))
+            
         # Connect to Cartesian Controller Action Server from Client
         self.cartesian_passthrough_trajectory_client = actionlib.SimpleActionClient(
             '/forward_cartesian_traj_controller/follow_cartesian_trajectory', FollowCartesianTrajectoryAction)
@@ -300,6 +315,13 @@ class UR():
         self.set_mode_client.wait_for_result()
         return self.set_mode_client.get_result().success
 
+    # Function to Load the Controller for UR Robot
+    def load_controller(self, controller_name):
+        srv = LoadControllerRequest()
+        srv.name = controller_name
+        result = self.load_controllers_client(srv)
+        print(result)
+      
     # Function to Switch the Controller for UR Robot
     def switch_on_controller(self, controller_name):
         """Switches on the given controller stopping all other known controllers with best_effort
